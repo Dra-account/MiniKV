a9328c65d0228fc8e7c640848d6dac2beec0e98f
Dra-account <maounfeng@qq.com>
2025-05-12 18:07:22 +0800

    feat:添加测试

---
+++ test/test_url.sh
@@ -0,0 +1,7 @@
+curl -X POST http://localhost:8080/api/query -d "set a 123"
+curl -X POST http://localhost:8080/api/query -d "get a"
+
+curl http://localhost:8080/api/health
+
+curl http://localhost:8080/
+curl http://localhost:8080/style.css
